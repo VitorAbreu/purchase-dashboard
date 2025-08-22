93afc79f41daace229fb77b198802107c946acf1
Vitor <vitoryanfma@gmail.com>
2025-08-21 22:45:19 -0300

    feat(tasks): making drawer sidenav behavior works, adding colours to status of tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component, effect, signal } from '@angular/core';
+import { AfterViewInit, Component, effect, inject, OnDestroy, signal, ViewChild } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +13,12 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy {
   theme = signal<'light' | 'dark'>(localStorage.getItem('theme') === 'dark' ? 'dark' : 'light');
+  breakpointObserver = inject(BreakpointObserver);
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  isDesktop: boolean = true;
+  private breakpointSub!: Subscription;
 
   constructor() {
     effect(() => {
@@ -22,7 +28,26 @@ export class AppComponent {
     });
   }
 
+  ngAfterViewInit(): void {
+    this.breakpointSub = this.breakpointObserver.observe(['(min-width: 1440px)']).subscribe((result) => {
+      setTimeout(() => {
+        this.isDesktop = result.matches;
+        if (this.isDesktop) {
+          this.sidenav.open();
+          this.sidenav.mode = 'side'; // always visible
+        } else {
+          this.sidenav.close();
+          this.sidenav.mode = 'over'; // overlays content
+        }
+      });
+    });
+  }
+
   toggleTheme(): void {
     this.theme.set(this.theme() === 'dark' ? 'light' : 'dark');
+  }
+
+  ngOnDestroy(): void {
+    this.breakpointSub.unsubscribe();
   }
 }
